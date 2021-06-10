aabdf0b6289b08f6a9011b4e6fac95c2ec11f338
Clayton Gilbert <claytongilbert@gmail.com>
2021-06-10 14:29:51 +1000

    Update Controller.kt

--- src/main/kotlin/com/flights/demo/Controller.kt
+++ src/main/kotlin/com/flights/demo/Controller.kt
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class Controller (val repo: FlightRepositories) {
 
+    /* add a new flight */
     @PostMapping( "/flight")
     fun addFlights(@RequestBody flight: Flight) {
         repo.save(flight)
     }
 
+    /* return all flights */
     @GetMapping("/flight")
     fun getAllFlights(): List<Flight> = repo.findAll().toList()
 
-    // return all flights by airline code
+    /* return all flights by airline code */
     @GetMapping("/flight/{airlineCode}")
     fun getFlightsByAirlineCode(@PathVariable("airlineCode") airlineCode : String): List<Flight> = repo.findFlightsByAirlineCode(airlineCode).toList()
 
